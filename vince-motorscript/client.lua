5a7b8cee0fa0411b4e04a7e9ca5f4094ee5a1c43
Vince <111222332+SenticAC@users.noreply.github.com>
2025-03-31 21:34:12 +0200

    Update client.lua
    Clean code

--- vince-motorscript/client.lua
+++ vince-motorscript/client.lua
@@ -1,69 +1,51 @@
-ESX = nil
-local cooldown = 10 * 1000 
-local lastSpawnTime = 0
+local Config = {
+    spawnPoints = {
+        vector3(-1043.3682, -854.8105, 4.8756)
+    },
+    model = "bati",
+    cooldown = 10000,
+    maxRange = 50.0
+}
+
+local ESX = nil
+local lastSpawn = 0
 
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) 
-            ESX = obj 
-        end)
-        Citizen.Wait(0)
+    while not ESX do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(100)
     end
 end)
 
-local SpawnPunten = {
-    vector3(-1043.3682, -854.8105, 4.8756), 
-}
-
-local VoertuigSpawner = {}
-VoertuigSpawner.__index = VoertuigSpawner
-
-function VoertuigSpawner.new(SpawnPunten)
-    local self = setmetatable({}, VoertuigSpawner)
-    self.SpawnPunten = SpawnPunten
-    return self
-end
-
-function VoertuigSpawner:SpawnMotor(speler, spelercoords)
+RegisterCommand("motor", function()
+    local player = PlayerPedId()
+    local coords = GetEntityCoords(player)
     local currentTime = GetGameTimer()
-    local canSpawn = (currentTime - lastSpawnTime) >= cooldown
-    local spawnPointGevonden = false
-
-    for _, spawnPunt in next, (self.SpawnPunten) do
-        local afstand = #(spelercoords - spawnPunt)
-        if afstand < 50.0 then
-            spawnPointGevonden = true
-            if canSpawn then
-                self:spawnVehicle(speler, spawnPunt)
-                exports['mythic_notify']:DoHudText('inform',  'Je hebt een motor gespawned!')
-                lastSpawnTime = currentTime
-            else
-                local remainingCooldown = math.ceil((lastSpawnTime + cooldown - currentTime) / 1000)
-                exports["mythic_notify"]:SendAlert("error", "Je moet " .. remainingCooldown .. " seconden wachten om een nieuwe motor te spawnen")
-            end
+    
+    if currentTime - lastSpawn < Config.cooldown then
+        local remaining = math.ceil((Config.cooldown - (currentTime - lastSpawn)) / 1000)
+        exports["mythic_notify"]:SendAlert("error", "Wacht nog " .. remaining .. " seconden")
+        return
+    end
+    
+    local spawnPoint = nil
+    for _, point in ipairs(Config.spawnPoints) do
+        if #(coords - point) < Config.maxRange then
+            spawnPoint = point
+            break
         end
     end
-
-    if not spawnPointGevonden then
-        exports["mythic_notify"]:SendAlert("error", "Je bent niet in de buurt van een spawnpoint")
+    
+    if not spawnPoint then
+        exports["mythic_notify"]:SendAlert("error", "Je bent niet bij een spawnpunt")
+        return
     end
-end
 
-function VoertuigSpawner:spawnVehicle(speler, spawnPunt)
-    local motorcycleModel = "bati"
-    local heading = GetEntityHeading(speler)
-    ESX.Game.SpawnVehicle(motorcycleModel, spawnPunt, heading, function(vehicle) 
+    ESX.Game.SpawnVehicle(Config.model, spawnPoint, GetEntityHeading(player), function(vehicle)
         if DoesEntityExist(vehicle) then
-            TaskWarpPedIntoVehicle(speler, vehicle, -1)
-            lastSpawnTime = GetGameTimer()
+            TaskWarpPedIntoVehicle(player, vehicle, -1)
+            lastSpawn = currentTime
+            exports['mythic_notify']:DoHudText('inform', 'Motor gespawned!')
         end
     end)
-end
-
-local voertuigSpawner = VoertuigSpawner.new(SpawnPunten)
-
-RegisterCommand("motor", function()
-    local speler = PlayerPedId()
-    local spelercoords = GetEntityCoords(speler)
-    voertuigSpawner:SpawnMotor(speler, spelercoords)
 end, false)
